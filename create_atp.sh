8448dc4afb356135f1330fd5d5bc8cf8a82d90b9
Mika Rinne <mika.rinne@oracle.com>
2022-12-01 10:52:00 +0100

    create

--- create_atp.sh
+++ create_atp.sh
@@ -14,7 +14,7 @@ atp_status=''
 while [ $tries -le 30 ] && [[ $atp_status != 'Active' ]] 
 do
   i=$(( (i+1) %4 ))
-  printf "\r${spin:$i:1}
+  printf "\r${spin:$i:1}"
   atp_status=$(kubectl get AutonomousDatabases -n ${mesh_name} -o json | jq '.items[].status.status.conditions[].type')
   if [ "$atp_status" != "" ]; then
    atp_status=$(kubectl get AutonomousDatabases -n ${mesh_name} -o json | jq '.items[].status.status.conditions[] | select(."type" == "Active") | .type' | tr -d '"')
@@ -24,4 +24,6 @@ do
 done
 if [ -z "$atp_status" ]; then
   echo "ATP instance $1 does not exist/could not be created .. "
+else
+  echo "ATP instance $1 running."
 fi
