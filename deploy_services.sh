2b4db444cfb738caa6ac8231c0bdac679241681b
Mika Rinne <mika.rinne@oracle.com>
2022-12-01 16:03:29 +0100

    create

--- deploy_services.sh
+++ deploy_services.sh
@@ -2,7 +2,6 @@
 if [ -z "$1" ]; then
   exit
 fi
-sed -i "s/tag/${mesh_version}/g" app.yaml
 sed -i "s/mesh_name/${mesh_name}/g" app.yaml
 sed -i "s|meshdemo_registry|$1|g" app.yaml
 kubectl label namespace $mesh_name servicemesh.oci.oracle.com/sidecar-injection=enabled
