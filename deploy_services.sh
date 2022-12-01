00859a9fa96f823c2e8118af4a93416fbe7ced59
Mika Rinne <mika.rinne@oracle.com>
2022-12-01 15:54:04 +0100

    create

--- deploy_services.sh
+++ deploy_services.sh
@@ -1,12 +1,9 @@
 # $1 = OCI registry
-# $1 = docker username
-# $2 = docker password (api key)
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ]; then
   exit
 fi
 sed -i "s/tag/${mesh_version}/g" app.yaml
 sed -i "s/mesh_name/${mesh_name}/g" app.yaml
 sed -i "s|meshdemo_registry|$1|g" app.yaml
 kubectl label namespace $mesh_name servicemesh.oci.oracle.com/sidecar-injection=enabled
-kubectl create secret docker-registry ocirsecret -n $mesh_name --docker-server $1 --docker-username $2 --docker-password $3
 kubectl create -f app.yaml
