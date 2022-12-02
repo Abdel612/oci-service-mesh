b2c2a53d3e4c1204c00d4e4f193b0e525abe25ff
Mika Rinne <mika.rinne@oracle.com>
2022-12-02 14:21:20 +0100

    create

--- create_monitoring.sh
+++ create_monitoring.sh
@@ -3,7 +3,7 @@ sed -i "s/mesh_name/${mesh_name}/g" logconfig.json
 sed -i "s/log_object_ocid/${log_object_ocid}/g" logconfig.json
 sed -i "s/mesh_name/${mesh_name}/g" grafana.yaml
 groupList=`echo '{"groupList": ["'${logging_dynamicgroup_ocid}'"]}'`
-oci logging agent-configuration create --compartment-id ${mesh_compartment} --is-enabled true --service-configuration file://logconfig.json --display-name MeshDemoLoggingAgent --description "Custom agent config for ${mesh_name}" --group-association "${groupList}"
+oci logging agent-configuration create --compartment-id ${mesh_compartment} --is-enabled true --service-configuration file://logconfig.json --display-name ${mesh_name}MeshLoggingAgent --description "Custom agent config for ${mesh_name}" --group-association "${groupList}"
 kubectl create ns monitoring
 kubectl apply -f prometheus.yaml
 kubectl apply -f grafana.yaml
