ec6a332c0c6eb1ca04f6fb081f8d788a8850a562
Mika Rinne <mika.rinne@oracle.com>
2022-12-02 14:01:46 +0100

    create

--- create_monitoring.sh
+++ create_monitoring.sh
@@ -1,14 +1,15 @@
 # ${log_object_ocid} = LOG object ocid # See https://docs.oracle.com/en-us/iaas/Content/service-mesh-tutorials/service-mesh-overview/00-overview.htm
 oci logging agent-configuration list --all --compartment-id ${mesh_compartment} | jq '.data.items[] | select(."lifecycle-state" == "ACTIVE") | .id' | tr -d '"' > delete.out
 while read line; do
-    echo "$line"
-    oci logging agent-configuration delete --config-id $line --force
+  echo "$line"
+  oci logging agent-configuration delete --config-id $line --force
 done <delete.out
 sleep 5
 sed -i "s/mesh_name/${mesh_name}/g" logconfig.json
 sed -i "s/log_object_ocid/${log_object_ocid}/g" logconfig.json
 sed -i "s/mesh_name/${mesh_name}/g" grafana.yaml
-export groupList=`echo '{"groupList": ["'${logging_dynamicgroup_ocid}'"]}'` oci logging agent-configuration create --compartment-id ${compartment_ocid} --is-enabled true --service-configuration file://logconfig.json --display-name MeshDemoLoggingAgent --description "Custom agent config for ${mesh_name}" --group-association "${groupList}"
+groupList=`echo '{"groupList": ["'${logging_dynamicgroup_ocid}'"]}'`
+oci logging agent-configuration create --compartment-id ${mesh_compartment} --is-enabled true --service-configuration file://logconfig.json --display-name MeshDemoLoggingAgent --description "Custom agent config for ${mesh_name}" --group-association "${groupList}"
 #kubectl delete ns monitoring
 kubectl create ns monitoring
 kubectl apply -f prometheus.yaml
