eda2e0c1b522adcda52c7fc8af9ae865eb57247c
Mika Rinne <mika.rinne@oracle.com>
2022-12-03 06:00:19 +0100

    create

--- update_dns.sh
+++ update_dns.sh
@@ -11,12 +11,7 @@ if [ -n "${dns_compartment}" ]; then
   compartment=${dns_compartment}
 fi
 name=$1.$2
-zone=$(oci dns record domain get --domain ${name} --zone-name-or-id $2 -c ${compartment} --region $3 | jq '.data.items[] | select(."domain" == "'${name}'") | .domain ' | tr -d '"')
-echo "zone: $zone"
-echo "name: $name"
-if [ "$zone" != "${name}" ]; then
- oci dns zone create -c ${compartment} --name $2 --zone-type 'PRIMARY' --region $3
-fi
+oci dns zone create -c ${compartment} --name $2 --zone-type 'PRIMARY' --region $3
 export items=`echo '[{ "domain": "'${name}'","is-protected": false,"rdata": "'$4'","rrset-version": "2","rtype": "A","ttl": 1800 }]'`
 echo $items
 oci dns record domain update --domain ${name} --zone-name-or-id ${dns_domain} -c ${compartment} --items="${items}" --region $3 --force
